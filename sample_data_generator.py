9bbf594490a84432ace2e60d83e5e759cd0e14f3
ask363 <ask363@usask.ca>
2023-03-12 20:37:35 -0600

    completed all functions, but has an error

--- sample_data_generator.py
+++ sample_data_generator.py
@@ -4,7 +4,7 @@ import openpyxl
 import random
 
 
-# used in generate_day_sale_data
+# Define function to get total sales of a given day
 def get_daily_total(weekday_sale_distribution_dataframe, weekday_name):
     # Filter dataframe to get all rows for the given weekday
     weekday_data = weekday_sale_distribution_dataframe[weekday_sale_distribution_dataframe['day'] == weekday_name]
@@ -23,9 +23,10 @@ def get_daily_total(weekday_sale_distribution_dataframe, weekday_name):
 
     return total
 
-# used in get_distribution_of_sale_among_hours which is used in generate_day_sale_data
+
+# Define function to divide total sale numbers among hours
 def divide_sale_num_among_hours(total, parts):
-    # Generate a list of random integers between 1 and total - 1
+    # Generate a list of random integers between 1 and 'total - 1'
     divisors = sorted(random.sample(range(1, total), parts - 1))
     # Calculate the differences between adjacent divisors and append total
     # to the list to get a list of parts that add up to total
@@ -34,6 +35,7 @@ def divide_sale_num_among_hours(total, parts):
     return parts_list
 
 
+# Define function to get sale distribution among hours
 def get_distribution_of_sale_among_hours(sale_per_time_of_day_dataframe, total_sale_num):
     distribution = {}
     for _, row in sale_per_time_of_day_dataframe.iterrows():
@@ -43,6 +45,7 @@ def get_distribution_of_sale_among_hours(sale_per_time_of_day_dataframe, total_s
         end_hour = end_hour_plus_1 - 1
         num_of_sales = total_sale_num * float(sale_percentage)/ 100
         int_num_of_sales = int(num_of_sales)
+        # Divide total sale numbers among hours
         divided_sale_num_list = divide_sale_num_among_hours(int_num_of_sales, num_hours)
         counter = 0
         while num_hours !=0:
@@ -52,12 +55,89 @@ def get_distribution_of_sale_among_hours(sale_per_time_of_day_dataframe, total_s
     return distribution
 
 
+# Define function to generate number of products for a sale
+def generate_number_of_products(product_quantity_probability_dataframe):
+    options = product_quantity_probability_dataframe['Product quantity'].tolist()
+    option_probabilities = product_quantity_probability_dataframe['Probability'].tolist()
+    # Use the random.choices() method to randomly choose an option based on the probabilities
+    chosen_option = random.choices(options, weights=option_probabilities)[0]
+    if len(chosen_option)==1:
+        return int(chosen_option)
+    else:
+        min_option = int(chosen_option[0])
+        max_option = int(chosen_option[2:])
+        chosen_number = random.randint(min_option, max_option)
+        return chosen_number
+
+
+# Define function to generate a random item from menu
+def generate_item(item_sale_probability_dataframe):
+    options = item_sale_probability_dataframe['Item'].tolist()
+    option_probabilities = item_sale_probability_dataframe['Probability'].tolist()
+    # Use the random.choices() method to randomly choose an option based on the probabilities
+    chosen_option = random.choices(options, weights=option_probabilities)[0]
+    return chosen_option
+
+
+# Define function to generate the details of a single sale
+def generate_single_sale(id_of_sale):
+    single_sale = []
+    items = {}
+    num_products = generate_number_of_products(product_quantity_probability_data)  # get the number of products for the sale using product_quantity_probability_data
+    for i in range(num_products):
+        item = generate_item(item_sale_probability_data)  # randomly choose an item from the menu using item_sale_probability_data
+        if item in items.keys():
+            items[item] += 1
+        else:
+            items[item] = 1
+    for product in items.keys():
+        single_sale.append([product, items[product]])
+    id_of_sale += 1
+
+    return single_sale, id_of_sale
+
+
+# Define function to get the price of an item from the menu
+def get_price(menu_dataframe, item_list):
+    single_price = menu_dataframe.loc[menu_dataframe['item_name']==item_list[0], 'price' ].values[0]
+    price = float(single_price)*int(item_list[1])
+
+    return price
+
+
 # Define function to generate sales data for an hour
 def generate_hour_sale_data(sale_id, date_obj, hour, total_sale_of_hour):
-    pass
+    time_instances = []
+    while len(time_instances) < int(total_sale_of_hour):
+        random_num = random.randint(0, 59)
+        if random_num not in time_instances:
+            time_instances.append(random_num)
+
+    time_instances.sort()
+    hour_data = []
+    for item in time_instances:
+
+        single_sale, sale_id = generate_single_sale(sale_id)
+        for an_item in single_sale:
+
+            if item >= 10:
+                complete_representation = [sale_id-1, date_obj.strftime("%Y-%m-%d"), str(hour) + ":" + str(item), "", "",
+                                           ""]
+            else:
+                complete_representation = [sale_id-1, date_obj.strftime("%Y-%m-%d"), str(hour) + ":" + '0' + str(item),
+                                           "", "", ""]
+
+            price = get_price(menu_data, an_item)
+            complete_representation[3] = an_item[0]
+            complete_representation[4] = an_item[1]
+            complete_representation[5] = price
+            hour_data.append(complete_representation)
+
+    return hour_data, sale_id
+
 
 # Define function to generate sales data for a day
-def generate_day_sale_data(sale_id, date):
+def generate_day_sale_data(sale_id_, date):
     hour = 10
     day_data = []
     day_of_week = date.strftime('%A')  # get day of the week for the given date
@@ -66,10 +146,10 @@ def generate_day_sale_data(sale_id, date):
     while hour != 22:
         hourly_total = distribution_of_sale_among_hours[hour]
         hour_data, sale_id_ = generate_hour_sale_data(sale_id_, date, hour, hourly_total)
-        day_data.append(hour_data)
+        for item in hour_data:
+            day_data.append(item)
         hour += 1
 
-    print(day_data)
     return pd.concat(day_data), sale_id_
 
 
